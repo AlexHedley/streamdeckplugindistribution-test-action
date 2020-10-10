049764a2742dbfc8c4e1e3bd9b454f161fe0c57a
Alex Hedley <alex.hedley@me.com>
2020-10-10 10:23:58 +0100

    Fixed error with no async/await

--- WORKINGS.md
+++ WORKINGS.md
@@ -11,3 +11,13 @@ npm install @actions/github
 ```bash
 npm install @actions/tool-cache
 ```
+
+Install the module.
+Add to script
+Push code
+add tag and push
+
+```bash
+git tag -a -m "My third action release" v0.3
+git push --follow-tags
+```
--- src/index.js
+++ src/index.js
@@ -4,36 +4,59 @@ const tc = require("@actions/tool-cache");
 const io = require('@actions/io');
 const os = require('os');
 
-try {
-    console.log("Running action");
-    core.debug('debug message');
-    core.warning('warning message');
-    core.info('info message');
 
-    core.info(`Process Platform: ${process.platform}`);
+///
+/// Download Distribution Tool
+///
+async function downloadDistributionTool () {
 
-    const homedir = os.homedir();
-    core.info(`homedir: ${homedir}`);
-    //await io.mkdirP('path/to/make');
-
-    if (process.platform === 'win32') {
-        core.info('attempting download of win dist tool');
-        
-        const distributionToolPath = await tc.downloadTool(' https://developer.elgato.com/documentation/stream-deck/distributiontool/DistributionToolWindows.zip');
-        const distributionToolExtractedFolder = await tc.extractZip(distributionToolPath, `${homedir}/disttool`);
+    try {
+        console.log("Running action");
+        core.debug('debug message');
+        core.warning('warning message');
+        core.info('info message');
+    
+        core.info(`Process Platform: ${process.platform}`);
+    
+        const homedir = os.homedir();
+        core.info(`homedir: ${homedir}`);
+        //await io.mkdirP('path/to/make');
+    
+        if (process.platform === 'win32') {
+            core.info('attempting download of win dist tool');
+            
+            const distributionToolPath = await tc.downloadTool(' https://developer.elgato.com/documentation/stream-deck/distributiontool/DistributionToolWindows.zip');
+            const distributionToolExtractedFolder = await tc.extractZip(distributionToolPath, `${homedir}/disttool`);
+        }
+        else if (process.platform === 'darwin') {
+            core.info('attempting download of mac dist tool');
+    
+            const distributionToolPath = await tc.downloadTool('https://developer.elgato.com/documentation/stream-deck/distributiontool/DistributionToolMac.zip');
+            const distributionToolExtractedFolder = await tc.extractXar(distributionToolPath, `${homedir}/disttool`);
+        }
+        else {
+            // Linux?
+            core.warning("is this linux? Isn't a tool to support this platform");
+        }
+    
+    } catch (error) {
+        core.error(`Error ${error}, action may still succeed though`);
+        core.setFailed(error.message);
     }
-    else if (process.platform === 'darwin') {
-        core.info('attempting download of mac dist tool');
 
-        const distributionToolPath = await tc.downloadTool('https://developer.elgato.com/documentation/stream-deck/distributiontool/DistributionToolMac.zip');
-        const distributionToolExtractedFolder = await tc.extractXar(distributionToolPath, `${homedir}/disttool`);
-    }
-    else {
-        // Linux?
-        core.warning("is this linux? Isn't a tool to support this platform");
-    }
-
-} catch (error) {
-    core.error(`Error ${error}, action may still succeed though`);
-    core.setFailed(error.message);
 }
+/// RUN
+
+async function run () {
+    core.info('run started');
+
+    try {
+        await downloadDistributionTool()
+    } catch (error) {
+        core.setFailed(error.message)
+    }
+
+    core.info('run finished');
+}
+
+run()
